6e371230ae2fdca4d7635c0c31e34416f161afaa
motwanikapil <motwanikapil1@gmail.com>
2021-06-06 13:10:44 +0530

    Router Section Started

---
+++ src/Checkout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Checkout() {
+    return (
+      <div className="checkout">
+        <div className="checkout__left">
+          <img
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/PostMayArt/AF_Header/AF-PC.jpg"
+            alt=""
+            className="checkout__ad"
+          />
+        </div>
+      </div>
+    );
+}
+
+export default Checkout
