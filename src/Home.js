a378e8769e34441593f6f9cf9c1d4a6be2187573
Kapil Motwani <51946007+motwanikapil@users.noreply.github.com>
2021-06-08 22:31:36 +0530

    home js

--- src/Home.js
+++ src/Home.js
@@ -10,12 +10,14 @@ function Home() {
         </div>
         <div className='home__row'>
           <Product
+            id='4903850'
             title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
             price={29.99}
             image='https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg'
             rating={5}
           />
           <Product
+            id='23445930'
             title="Children's Encyclopedia - Scientists, Inventions And Discoveries: The World of Knowledge"
             price={2.8}
             image='https://images-na.ssl-images-amazon.com/images/I/51Kuw4q549S._SX355_BO1,204,203,200_.jpg'
@@ -24,18 +26,21 @@ function Home() {
         </div>
         <div className='home__row'>
           <Product
+            id='3254354345'
             title='DECORVAIZ Multipurpose Laptop Table with Dock Stand & Non-Slip Legs Foldable and Portable Lapdesk for Study & Bed'
             price={2.89}
             image='https://images-na.ssl-images-amazon.com/images/I/51k9OmMZyIS.jpg'
             rating={3}
           />
           <Product
+            id='32543543'
             title='Zebronics Zeb-Transformer-M Optical USB Gaming Mouse with LED Effect(Black)'
             price={29.99}
             image='https://images-na.ssl-images-amazon.com/images/I/81tioCUVf4L._SL1500_.jpg'
             rating={5}
           />
           <Product
+            id='325443'
             title='Lenovo Ideapad S145 AMD RYZEN 3 3200U 15.6-inch (39.62 cms) FHD Laptop (4GB/1TB/Windows 10/Office 2019/Grey/1.85Kg),81UT001CIN'
             price={80.0}
             image='https://images-na.ssl-images-amazon.com/images/I/811w2pSQcnL._SL1500_.jpg'
@@ -44,6 +49,7 @@ function Home() {
         </div>
         <div className='home__row'>
           <Product
+            id='3443'
             title='Onida 108 cm (43 Inches) Fire TV Edition Full HD Smart IPS LED TV 43FIF (Black) (2020 Model)'
             price={29.99}
             image='https://images-na.ssl-images-amazon.com/images/I/71vJoWfZDLL._SL1500_.jpg'
