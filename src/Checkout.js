42d24895c533c776d543c8559d9be662d0c7e49e
Kapil Motwani <51946007+motwanikapil@users.noreply.github.com>
2021-06-09 02:26:02 +0530

    Checkout Page Updated

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import './Checkout.css'
+import Subtotal from './Subtotal'
 
 function Checkout() {
     return (
-      <div className="checkout">
-        <div className="checkout__left">
+      <div className='checkout'>
+        <div className='checkout__left'>
           <img
-            src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/PostMayArt/AF_Header/AF-PC.jpg"
-            alt=""
-            className="checkout__ad"
+            src='https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/AMI/Survey/D23285344_IN_MobileInsider_Survey_1500x300_28.jpg'
+            alt=''
+            className='checkout__ad'
           />
+          <div>
+            <h2 className="checkout__title">Your Shopping Basket</h2>
+          </div>
+        </div>
+        <div className="checkout__right">
+          <Subtotal />
         </div>
       </div>
-    );
+    )
 }
 
 export default Checkout
