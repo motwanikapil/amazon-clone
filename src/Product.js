c5e4b1938e2bd3b0bc2d50b142054600570bbfcc
Kapil Motwani <51946007+motwanikapil@users.noreply.github.com>
2021-06-09 10:40:34 +0530

    Revert "Checkout Page Updated"
    This reverts commit 42d24895c533c776d543c8559d9be662d0c7e49e.

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,6 @@
 import React from 'react'
 import './Product.css'
-import {useStateValue} from './StateProvider'
-function Product({id,title,image,price,rating}) {
-  const [ state , dispatch] = useStateValue();
-  const addToBasket = () => {
-    // dispatch the item into the data layer
-      dispatch({
-        type : "ADD_TO_BASKET",
-        item : {
-          id : id,
-          title : title,
-          image : image,
-          price : price,
-          rating : rating,
-        }
-      });
-  };
+function Product({title,image,price,rating}) {
     return (
       <div className='product'>
         <div className='product__info'>
@@ -36,7 +21,7 @@ function Product({id,title,image,price,rating}) {
           src={image}
             alt=""
         />
-        <button onClick={addToBasket}>Add to Basket</button>
+        <button>Add to Basket</button>
       </div>
     )
 }
